c65723572180a4b65f05d6bb24621bd3de61d171
Louis Dionne <louis.dionne92@gmail.com>
2012-05-03 00:29:14 -0400

    Add type traits module.
    Type traits are in many ways similar to the type traits of Boost. Basically,
    they provide metainformation on types to allow a finer grained customization
    of parametric code.

---
+++ include/joy/internal/compare.h
@@ -0,0 +1,36 @@
+/*!
+ * This file defines a macro to compare identifiers for (in)equality.
+ */
+
+#ifndef JOY_INTERNAL_COMPARE_H
+#define JOY_INTERNAL_COMPARE_H
+
+# include <chaos/preprocessor/control/iif.h>
+# include <chaos/preprocessor/detection/is_nullary.h>
+# include <chaos/preprocessor/logical/bitand.h>
+# include <chaos/preprocessor/logical/compl.h>
+
+
+/*!
+ * Compare two identifiers with a common prefix.
+ *
+ * @note This is a (very) slightly modified version of the CHAOS_PP_COMPARE()
+ *          macro of the Chaos preprocessor library. All the credit goes to
+ *          the Chaos library for this macro.
+ */
+#define JOY_COMPARE(prefix, a, b)                                              \
+    CHAOS_PP_IIF(                                                              \
+        CHAOS_PP_BITAND                                                        \
+            (CHAOS_PP_IS_NULLARY(prefix ## a(())))                             \
+            (CHAOS_PP_IS_NULLARY(prefix ## b(())))                             \
+    )(                                                                         \
+        CHAOS_PP_COMPL(CHAOS_PP_IS_NULLARY(                                    \
+            prefix ## a(                                                       \
+                prefix ## b                                                    \
+            )(())                                                              \
+        )),                                                                    \
+        0                                                                      \
+    )                                                                          \
+/**/
+
+#endif /*! JOY_INTERNAL_COMPARE_H */
