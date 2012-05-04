e0a58bcecf15365a0b47a50aac70bcfd3e70a6cc
Louis Dionne <louis.dionne92@gmail.com>
2012-05-04 15:17:01 -0400

    Add the Ctype type.
    A Ctype is a preprocessor type with a real C representation. It can be
    declared and defined. This type is useful as a base type for all the other
    types that are actually used to simulate templates in C.

--- include/joy.h
+++ include/joy.h
@@ -11,5 +11,6 @@
 #include "joy/object.h"
 #include "joy/named_object.h"
 #include "joy/namespace.h"
+#include "joy/Ctype.h"
 
 #endif /* !JOY_H */
---
+++ include/joy/Ctype.h
@@ -0,0 +1,14 @@
+/*!
+ * Master header for the Ctype module of the Joy library.
+ *
+ * @author Louis Dionne
+ */
+
+#ifndef JOY_CTYPE_H
+#define JOY_CTYPE_H
+
+#include "Ctype/base.h"
+#include "Ctype/primitive.h"
+#include "Ctype/class.h"
+
+#endif /* !JOY_CTYPE_H */
