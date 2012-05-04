358ae1f9d3e5231d0e2f3e24be72c2657f17e162
Louis Dionne <louis.dionne92@gmail.com>
2012-05-04 15:24:55 -0400

    Add a type creator.
    This is the only metatype of the Joy hierarchy, in the sense that it creates
    all the other types.

---
+++ include/joy/type.h
@@ -0,0 +1,86 @@
+/*!
+ * This file defines an interface to create and manipulate types.
+ *
+ * @author Louis Dionne
+ */
+
+#ifndef JOY_TYPE_H
+#define JOY_TYPE_H
+
+#include "internal/map.h"
+#include "internal/strcmp.h"
+#include <chaos/preprocessor/tuple/elem.h>
+#include <chaos/preprocessor/recursion/expr.h>
+
+
+/*!
+ * Create a new type.
+ *
+ * @param name The name of the type.
+ * @param base The base type of the type.
+ *              A type inherits all the attributes of its base type.
+ * @param attrs A sequence of behavior:implementation tuples containing the
+ *              attributes of the type. These attributes usually are macros
+ *              applicable to an instance of the type.
+ */
+#define JOY_TYPE(name, base, attrs) \
+    JOY_TYPE_S(CHAOS_PP_STATE(), name, base, attrs)
+
+#define JOY_TYPE_S(state, name, base, attrs) \
+    (name, base, JOY_I_TYPE_MAKEATTRS(state, base, attrs))
+#define JOY_TYPE_S_ID() JOY_TYPE_S
+
+#define JOY_I_TYPE_MAKEATTRS(state, base, new_attrs)                           \
+    JOY_MAP_UPDATE_S(state,                                                    \
+        JOY_TYPE_ATTRSOF(base),                                                \
+        JOY_SEQ_TO_MAP_S(state, JOY_I_TYPE_LESS, new_attrs)                    \
+    )                                                                          \
+/**/
+
+#define JOY_I_TYPE_LESS(state, a, b) \
+    JOY_MAP_KEY_COMP_ADJUST(JOY_STRING_LESS_S, state, a, b)
+
+/*!
+ * The empty type. This is only useful as a base for the first type of the
+ * Joy hierarchy.
+ */
+#define JOY_TYPE_NIL ( , , JOY_MAP(JOY_I_TYPE_LESS))
+#define JOY_TYPE_NIL_ID() JOY_TYPE_NIL
+
+/*!
+ * This macro is used when a macro should be implemented in a derived type.
+ *
+ * It will trigger a preprocessor error whenever called with more than 0
+ * arguments.
+ */
+#define JOY_NOT_IMPLEMENTED()
+#define JOY_NOT_IMPLEMENTED_ID() JOY_NOT_IMPLEMENTED
+
+/*!
+ * Retrieve the name of a type.
+ */
+#define JOY_TYPE_NAMEOF(type) CHAOS_PP_TUPLE_ELEM(3, 0, type)
+
+/*!
+ * Retrieve the base type of a type.
+ */
+#define JOY_TYPE_BASEOF(type) CHAOS_PP_TUPLE_ELEM(3, 1, type)
+
+/*!
+ * Retrieve the attributes of a type as a map.
+ */
+#define JOY_TYPE_ATTRSOF(type) CHAOS_PP_TUPLE_ELEM(3, 2, type)
+
+/*!
+ * Retrieve the value of an attribute of a type.
+ *
+ * @internal Call the result of the search because we store only IDs. Calling
+ *              the ID will give the actual value of the attribute.
+ */
+#define JOY_TYPE_GETATTR(type, attr) \
+    JOY_TYPE_GETATTR_S(CHAOS_PP_STATE(), type, attr)
+
+#define JOY_TYPE_GETATTR_S(state, type, attr) \
+    JOY_MAP_FIND_E_S(state, JOY_TYPE_ATTRSOF(type), attr) /*ID*/()
+
+#endif /* !JOY_TYPE_H */
