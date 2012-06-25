c6afa184525de89d5dc8ad045b850d0c8e7c47d8
Louis Dionne <louis.dionne92@gmail.com>
2012-06-25 11:42:33 -0400

    Add a master header for the whole library.

---
+++ include/joy.h
@@ -0,0 +1,29 @@
+/*!
+ * Master header of the joy library.
+ *
+ * @author Louis Dionne
+ */
+
+#ifndef JOY_H
+#define JOY_H
+
+#include <joy/arglist.h>
+#include <joy/array.h>
+#include <joy/bnode.h>
+#include <joy/char.h>
+#include <joy/compare.h>
+#include <joy/execute.h>
+#include <joy/macro.h>
+#include <joy/mangle.h>
+#include <joy/map.h>
+#include <joy/object.h>
+#include <joy/pair.h>
+#include <joy/parse.h>
+#include <joy/search_result.h>
+#include <joy/seq.h>
+#include <joy/set.h>
+#include <joy/string.h>
+#include <joy/tree.h>
+#include <joy/tuple.h>
+
+#endif /* !JOY_H */
